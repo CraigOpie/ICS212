dedb0b58d10ae566cb1f35380b23fbc352f3c54c
Craig Opie <CraigOpie@Gmail.com>
2020-01-28 18:54:00 -1000

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   homework1/Exercise1/homework1
    	modified:   homework1/Exercise1/homework1.c
    	modified:   homework1/Exercise1/homework1.o
    	new file:   homework1/Exercise1/output.txt
    	new file:   homework1/Exercise1/proof.txt
    	new file:   homework1/Exercise2/exercise
    	modified:   homework1/Exercise2/exercise.c
    	new file:   homework1/Exercise2/exercise.o
    	new file:   homework1/submital/Exercise1/homework1
    	new file:   homework1/submital/Exercise1/homework1.c
    	new file:   homework1/submital/Exercise1/homework1.o
    	new file:   homework1/submital/Exercise1/output.txt
    	new file:   homework1/submital/Exercise1/proof.txt
    	new file:   homework1/submital/Exercise2/report.txt
    	new file:   homework1/submital/Exercise2/table.csv
    	new file:   homework1/submital/homework1.zip

--- homework1/Exercise2/exercise.c
+++ homework1/Exercise2/exercise.c
@@ -4,10 +4,21 @@ void dummy(int, char, char *, float);
 
 int main(int argc, char* argv[])
 {
+    printf("argc = %d\n", argc);
+    printf("%x argc = %p\n", &argc, &argc);
+    printf("%x argv = %p\n", &argv[0], &argv[0]);
+    printf("%x argv w/p = %p\n", argv[0], argv[0]);
     int num1, num2 = 5;
+    printf("%x num1 = %p\n", &num1, &num1);
+    printf("%x num2 = %p\n", &num2, &num2);
     char c = 'A';
+    printf("%x c = %p\n", &c, &c);
     float score1 = 25.5;
+    printf("%x score1 = %p\n", &score1, &score1);
     char ca[3] = "Hi";
+    printf("%x ca[0] = %c\n", &ca[0], ca[0]);
+    printf("%x ca[1] = %c\n", &ca[1], ca[1]);
+    printf("%x ca[2] = %c\n", &ca[2], ca[2]);
 
     dummy(num2, c, ca, score1);
 
@@ -18,7 +29,15 @@ int main(int argc, char* argv[])
 
 void dummy(int x, char y, char * z, float w)
 {
+    printf("%x x = %p\n", &x, &x);
+    printf("%x y = %p\n", &y, &y);
+    printf("%x *z = %p\n", &z, &z);
+    printf("%x *z w/p = %p\n", z, z);
+    printf("%x w = %p\n", &w, &w);
     x++;
+    printf("%x x = %p\n", &x, &x);
     y++;
+    printf("%x y = %c\n", &y, y);
     w = w + 5.1;
+    printf("%x w = %.2f\n", &w, w);
 }
