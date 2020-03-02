e5ca56edf1796c8db21527aade646e4109b1f9e5
CraigOpie <CraigOpie@Gmail.com>
2020-03-01 23:05:05 -1000

    test1

--- homework5/iofunctions.c
+++ homework5/iofunctions.c
@@ -82,22 +82,174 @@ int readfile( struct record accarray[], int * numcust, char filename[] )
             item = 1;
             i++;
         }
+        accountnoC[79] = '\0'
         if (item = 1)
         {
             fgets(accarray[count].name, 25, file_p);
             item = 2;
             i++;
         }
+        accarray[count].name[24] = '\0'
         if (item = 3)
         {
             fgets(accarray[count].address, 80, file_p);
             item = 0;
             i++;
-            count++;
         }
+        accarray[count].address[79] = '\0'
+        count++;
     }
+    numcust = lines / 3;
 
     return success;
+}
+
+/******************************************************************************
+//
+//  FUNCTION NAME: writefile
+//
+//  DESCRIPTION:   A function used to write to files
+//
+//  PARAMETERS:    None
+//
+//  RETURN VALUES: 0 : success
+//
+******************************************************************************/
+
+int writefile( struct record accarray[], int numcust, char filename[] )
+{
+    int success = 0;
+    int count = 0;
+    int item = 0;
+    int i = 0;
+    char currentChar = '\0';
+
+    FILE * file_p;
+    int accuntno;
+    char name[25];
+    char address[80];
+
+    file_p = fopen('outfile.txt', "w");
+    if (file_p == NULL)
+    {
+        success = -1;
+    }
+
+    while (currentChar != EOF)
+    {
+        if (item = 0)
+        {
+            fprintf(file_p, accarray[count].accountno + '\n', text);
+            item = 1;
+        }
+        if (item = 1)
+        {
+            i = 0;
+            while (currentChar != '\n')
+            {
+                currentChar = accarray[count].name[i];
+                fprintf(file_p, currentChar, text);
+                i++;
+            }
+            item = 2;
+        }
+        if (item = 2)
+        {
+            i = 0;
+            while (currentChar != '\n')
+            {
+                currentChar = accarray[count].address[i];
+                fprintf(file_p, currentChar, text);
+                i++;
+            }
+            item = 0;
+        }
+        count++;
+    }
+
+    fclose(file_p);
+
+    return success;
+}
+
+/******************************************************************************
+//
+//  FUNCTION NAME: strToInt
+//
+//  DESCRIPTION:   Takes a string input and performs checks to ensure it is an
+//                 integer value, then converts the useable string values to an
+//                 integer.  The value is returned for use.
+//
+//  PARAMETERS:    None.
+//
+//  RETURN VALUES: int : Value inputed.
+//
+******************************************************************************/
+
+int strToInt()
+{
+    char user_input[80];
+    int i;
+    int size_user_input = 0;
+    int bad_input = 1;
+    int digits;
+    int valid_int;
+
+    do
+    {
+        fgets(user_input, 80, stdin);
+        for (i = 0; i < 80; i++)
+        {
+            if (((int)user_input[i] > 47) && ((int)user_input[i] < 58))
+            {
+                user_input[size_user_input] = user_input[i];
+                size_user_input++;
+            }
+        }
+        for (i = (size_user_input - 1); i < 80; i++)
+        {
+            user_input[i] = '\0';
+        }
+        for (i = 0; i < 80; i++)
+        {
+            if ((int)user_input[i] == 3)
+            {
+                user_input[i] = '\0';
+            }
+        }
+        digits = 0;
+        valid_int = 0;
+        for (i = (size_user_input - 1); i >= 0; i--)
+        {
+            if (((int)user_input[i] > 47) && ((int)user_input[i] < 58))
+            {
+                bad_input = 0;
+                valid_int += (exponent(10, digits) * ((int)user_input[i] - 48));
+                digits++;
+            }
+        }
+        if (((int)user_input[0] == 45) || (valid_int <=0))
+        {
+            bad_input = 1;
+        }
+        for (i = 0; i < size_user_input; i++)
+        {
+            if ((int)user_input[i] == 46)
+            {
+                bad_input = 1;
+            }
+        }
+        if (bad_input == 1)
+        {
+            for (i = 0; i < 80; i++)
+            {
+                user_input[i] = '\0';
+            }
+        }
+    }
+    while(bad_input == 1);
+
+    return(valid_int);
 }
 
 /******************************************************************************
