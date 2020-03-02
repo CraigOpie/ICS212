a51251b328b300d858b1b8504144b8d91b6483c4
CraigOpie <CraigOpie@Gmail.com>
2020-03-02 00:36:50 -1000

    added driver file

---
+++ homework5/driver.c
@@ -0,0 +1,44 @@
+/*******************************************************************************
+//
+//  NAME:          Craig Opie
+//
+//  HOMEWORK:      5
+//
+//  CLASS:         ICS 212
+//
+//  INSTRUCTOR     Ravi Narayan
+//
+//  DATE:          Feb 29, 2020
+//
+//  FILE:          iofunctions.c
+//
+//  DESCRIPTION:   This file contains input/output functions.
+//
+//
+******************************************************************************/
+
+#include <stdio.h>
+#include "iofunctions.h"
+
+/******************************************************************************
+//
+//  FUNCTION NAME: main
+//
+//  DESCRIPTION:   A function used to test the read and write functions
+//
+//  PARAMETERS:    None
+//
+//  RETURN VALUES: 0 : success
+//
+******************************************************************************/
+
+int main( int argc, char argv[] )
+{
+    struct record accarray[25];
+    int numcust;
+
+    readfile(accarray, &numcust, "test.txt");
+    writefile(accarray, numcust, "outfile.txt");
+
+    return 0;
+}
