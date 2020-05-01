b86efb315412eed641a502ed3df0e9425f697f28
CraigOpie <CraigOpie@Gmail.com>
2020-05-01 07:07:32 -1000

    framework

---
+++ homework9/homework9.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+//
+//  NAME:           Craig Opie
+//
+//  HOMEWORK:       8
+//
+//  CLASS:          ICS 212
+//
+//  INSTRUCTOR:     Ravi Narayan
+//
+//  DATE:           Mar 30, 2020
+//
+//  FILE:           homework8.c
+//
+//  DESCRIPTION:    This program uses a function containing a for-loop to
+//                  generate a list of inches vs centimeters, starting from 0
+//                  inches to the user provided numer, with an interval of 0.50
+//                  inches.
+//
+******************************************************************************/
+
+#include <stdio.h>
+#include "jni.h"
+#include "jni_md.h"
+
+JNIEXPORT void JNICALL
+
+convert(JNIEnv *env, jobject obj)
+{
+    return = (inch * 2.54);
+}
